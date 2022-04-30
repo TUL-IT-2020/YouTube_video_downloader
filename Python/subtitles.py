d4c3d265d0daa1957c03179ad38a4f12988096d5
elPytel <jaroslav.korner1@gmail.com>
2022-04-30 17:30:29 +0200

    add youtube_transcript_api lib api

--- Python/subtitles.py
+++ Python/subtitles.py
@@ -1,42 +1,58 @@
 from youtube_transcript_api import YouTubeTranscriptApi as YTTA
 
-class InvalidInput(Exception):
-    """
-    Invalid input exception.
-    """
+class InvalidLanguage(Exception):
+    pass
 
-def transcript_has_language(transcripts, language):
-    for transcript in transcripts:
-        if transcript['title'] == language['title']:
-            return True
-    return False
-
-def transcript_get_languages(transcripts):
-    languages = []
-    for transcript in transcripts:
-        languages.append(transcript['title'])
-    return languages
 
 class Subtitles():
     def __init__(self, id, language=None):
         self.id = id
-        if language==None:
-            self.languages = YTTA.list_transcripts(id)
-        else:
-            self.text = YTTA.get_transcript(id, languages=[language])
+        self.language = language
+        self.transcripts = YTTA.list_transcripts(id)
+        self.get_language(language)
 
-    def save(self, file):
-        if self.text is not None:
-            
+    def has_language(self, language):
+        for transcript in self.transcripts:
+            if transcript.language_code == language:
+                return True
+        return False
+
+    def get_language(self, language):
+        if language is not None and self.has_language(language):
+            self.text = YTTA.get_transcript(self.id, languages=[language])
+            #transcript.fetch()
+
+    def save(self, file=None, plain_text=False):
+        if self.text is None:
+            raise InvalidLanguage
+        if file is None:
+            file = self.id + "_" + self.language + ".txt"
+        
+        with open(file, "w") as f:
+            for line in self.text:
+                if plain_text:
+                    f.write(line[text]+"\n")
+                else:
+                    f.write("{}\n".format(line))
 
     def __str__(self):
-        return ("# Subitles: \tLanguage: " + self.title)
+        string = "id: \t\t Language: \t Code: \t Generated:\n"
+        for transcript in self.transcripts:
+            string += (""+
+                transcript.video_id + "\t " +
+                transcript.language + "\t " +
+                transcript.language_code + "\t " +
+                str(transcript.is_generated) + "\n"
+            )
+        return string
 
 if __name__ == '__main__':
     id = "bRUtDtMAVKQ"
+    languages = "cs"
     transcript_list = YTTA.list_transcripts(id)
-    print(transcript_list)
-    srt = YTTA.get_transcript(id, languages=['cs'])
-    print(srt)
-    srt = YTTA.get_transcript(id, languages=['en'])
-    print(srt)
+    sub = Subtitles(id)
+    print(sub)
+    sub = Subtitles(id, languages)
+    sub.save()
+    #srt = YTTA.get_transcript(id, languages=['en'])
+    #print(srt)
